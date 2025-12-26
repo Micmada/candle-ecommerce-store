20414f2b20f773d8e5bd1d25da95955ff40e79e6
MichaelE <michael.eddleston@icloud.com>
2025-12-26 22:00:30 +0000

    Update Frontend API URL

--- candle-shop-frontend/src/utils/api.js
+++ candle-shop-frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://pprhxpttpm.us-east-1.awsapprunner.com/api';
 
 export const testConnection = async () => {
   const response = await fetch(`${API_BASE_URL}/test`);
